bd6425120184f46ab8eb18e131049927cdd07a9a
Andy <121941283+Andytomsingvien@users.noreply.github.com>
2023-04-11 12:00:27 +0200

    Mise à jours du code, dernier ajout, (complétion de la BDD par l'Entity).

--- GestionEtudiants/src/main/java/com/example/gestionetudiants/Impl/ClasseRepository.java
+++ GestionEtudiants/src/main/java/com/example/gestionetudiants/Impl/ClasseRepository.java
@@ -4,6 +4,5 @@ import com.example.gestionetudiants.entity.Classe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ClasseRepository extends JpaRepository<Classe, Long> {
 }
